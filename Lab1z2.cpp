9b9887e26dcd7497e74a6aff3c1a2f75fd3ada00
kanashiftw <92251137+kanashiftw@users.noreply.github.com>
2022-09-07 16:44:31 +0300

    Update Lab1z2.cpp

--- Lab1z2.cpp
+++ Lab1z2.cpp
@@ -1,11 +1,11 @@
 #include <iomanip>
 #include <iostream>
 #include <sstream>
-const unsigned int arr_size_x = 5;
-const unsigned int arr_size_y = 5;
-unsigned int ary[arr_size_x][arr_size_y];
+const int arr_size_x = 5;
+const int arr_size_y = 5;
+int ary[arr_size_x][arr_size_y];
 
-long double fact(int N)
+int  fact(int N)
 {
     if (N < 0) 
         return 0; 
@@ -14,31 +14,29 @@ long double fact(int N)
     else 
         return N * fact(N - 1); 
 }
-int main()
-{
-    setlocale(LC_ALL, "Russian");
-    std::cout << "Заполненный массив:\n" << std::endl;
-    for (unsigned int i = 0; i < arr_size_x; i++) {
+void printArray(int array[arr_size_x][arr_size_y]) {
+     std::cout << "Заполненный массив:\n";
+    for (int i = 0; i < arr_size_x; i++) {
         std::cout << "\n";
-    for (unsigned int k = 0; k < arr_size_y; k++) {
-        ary[i][k] = fact(i + k);
+    for (int k = 0; k < arr_size_y; k++) {
+        array[i][k] = fact(i + k);
         if (k == 0) {
-            std::cout << ary[i][k];
+            std::cout << array[i][k];
         }
         else {
-            std::cout << std::setw(8) << ary[i][k];
+            std::cout << std::setw(8) << array[i][k];
         }
     }
-} 
-    std::cout << "\n";
-    std::cout << "\n";
+}
+    }
+void printnewArray(int array[arr_size_x][arr_size_y]) {
     std::cout << "Измененный массив:\n" << std::endl;
-    int solo [(arr_size_x * arr_size_y) - (2*arr_size_x)];
-    for (unsigned int i = 0; i < arr_size_x; i+=2) {
+    int solo[(arr_size_x * arr_size_y) - (2 * arr_size_x)];
+    for (unsigned int i = 0; i < arr_size_x; i += 2) {
         for (unsigned int k = 0; k < arr_size_y; k++) {
             if (i == 0 && k == 0) {
-              solo[i] = ary[i][k];
-              std::cout << solo[i];
+                solo[i] = ary[i][k];
+                std::cout << solo[i];
             }
             else {
                 solo[i] = ary[i][k];
@@ -46,5 +44,12 @@ int main()
             }
         }
     }
-
+}
+   
+int main()
+{
+    setlocale(LC_ALL, "Russian");
+    printArray(ary);
+    std::cout << "\n\n";
+    printnewArray(ary);
 }
