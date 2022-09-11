72d3ca08525b990fbf68bce7038980f9e204d2bf
kanashiftw <92251137+kanashiftw@users.noreply.github.com>
2022-09-11 11:57:41 +0300

    Update Lab1z2.cpp

--- Lab1z2.cpp
+++ Lab1z2.cpp
@@ -3,32 +3,36 @@
 #include <sstream>
 const int arr_size_x = 5;
 const int arr_size_y = 5;
-int ary[arr_size_x][arr_size_y];
+int ary[arr_size_x][arr_size_y];//Инициализация массива ary 
+//с размерами arr_size_x и arr_size_y
 
+//Метод вычисления факториала
 int  fact(int N)
 {
-    if (N < 0) 
-        return 0; 
-    if (N == 0) 
-        return 1; 
-    else 
-        return N * fact(N - 1); 
+    if (N < 0)
+        return 0;
+    if (N == 0)
+        return 1;
+    else
+        return N * fact(N - 1);
 }
+//Метод вывода массива
 void printArray(int array[arr_size_x][arr_size_y]) {
-     std::cout << "Заполненный массив:\n";
+    std::cout << "Заполненный массив:\n";
     for (int i = 0; i < arr_size_x; i++) {
         std::cout << "\n";
-    for (int k = 0; k < arr_size_y; k++) {
-        array[i][k] = fact(i + k);
-        if (k == 0) {
-            std::cout << array[i][k];
-        }
-        else {
-            std::cout << std::setw(8) << array[i][k];
+        for (int k = 0; k < arr_size_y; k++) {
+            array[i][k] = fact(i + k);
+            if (k == 0) {
+                std::cout << array[i][k];
+            }
+            else {
+                std::cout << std::setw(8) << array[i][k];
+            }
         }
     }
 }
-    }
+//Метод вывода результирующего массива согласно варианту
 void printnewArray(int array[arr_size_x][arr_size_y]) {
     std::cout << "Измененный массив:\n" << std::endl;
     int solo[(arr_size_x * arr_size_y) - (2 * arr_size_x)];
@@ -45,7 +49,7 @@ void printnewArray(int array[arr_size_x][arr_size_y]) {
         }
     }
 }
-   
+
 int main()
 {
     setlocale(LC_ALL, "Russian");
